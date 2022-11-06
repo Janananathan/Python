518b5bf36b33264450e360e7080beed6d96384d7
Janananathan <janananathan@gmail.com>
2022-11-06 19:41:28 +0530

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,17 @@
+def addition(x,y):
+    return x+y
+
+def sybstract(x,y):
+    if x>=y:
+        return x-y
+    else:
+        return y-x
+
+def multiplication(x,y):
+    return x*y
+
+def division(x,y):
+    return x/y
+
+
+
