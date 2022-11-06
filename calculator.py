4d07e89a0d129b082877966f182ca311d7d9b44d
Janananathan <janananathan@gmail.com>
2022-11-06 19:58:37 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,25 @@
 import calculation as cal
-
-x=int(input("enter x="))
-print(x)
-y=int(input("enter y="))
-print(y)
-choice=int(input("Select\n1 for add\n2 for substract\n3 for multiply\n4 for division\nChoice : "))
-print(choice)
-if(choice ==1):
-    ad=cal.addition(x,y)
-    print("addition =",ad)
-elif choice==2:
-    ad=cal.sybstract(x,y)
-    print("substraction =",ad)
-elif choice==3:
-    ad=cal.multiplication(x,y)
-    print("mulriplication =",ad)
-elif choice==4:
-    ad=cal.division(x,y)
-    print("division =",ad)
-else:
-    print("wrong choice, existing")
+choice = 0
+while choice == 0:
+    x = int(input("enter x="))
+    print(x)
+    y = int(input("enter y="))
+    print(y)
+    choice = int(input(
+        "Select\n1 for add\n2 for substract\n3 for multiply\n4 for division\nChoice : "))
+    print(choice)
+    if(choice == 1):
+        ad = cal.addition(x, y)
+        print("addition =", ad)
+    elif choice == 2:
+        ad = cal.sybstract(x, y)
+        print("substraction =", ad)
+    elif choice == 3:
+        ad = cal.multiplication(x, y)
+        print("mulriplication =", ad)
+    elif choice == 4:
+        ad = cal.division(x, y)
+        print("division =", ad)
+    else:
+        print("wrong choice, existing")
+    choice=int(input("Continue? press 0 or -1 :"))
