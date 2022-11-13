c3dd8740faa9d301f65b423d7cf73b970f412214
Janananathan <janananathan@gmail.com>
2022-11-13 19:24:12 +0530

    Update num_1.py

--- num_1.py
+++ num_1.py
@@ -3,3 +3,10 @@ import numpy as np
 my=[1,2,3,46,66]
 npl=np.asarray(my)
 print(npl)
+kp=np.random.random((3,3))
+print(kp)
+print(my[::-1])
+
+myle=np.arange(20)
+np_2dl=np.reshape(myle,(5,4))
+print(np_2dl)
